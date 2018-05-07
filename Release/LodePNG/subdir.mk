a108b4b8fd2ee4d64f5c78f573d27e866a07d062
Luyao Tian <contact@thisisluyaotian.me>
2018-05-07 05:48:37 -0600

    Enabled mipmapping in experimental

--- Release/LodePNG/subdir.mk
+++ Release/LodePNG/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 LodePNG/%.o: ../LodePNG/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -D_RELEASE -U_DEBUG -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -D_RELEASE -U_DEBUG -O3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
